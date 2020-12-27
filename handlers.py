d3c1627454eb3703114135e55428a7134bcf02b5
Jhon-G <74727160+Jhon-G@users.noreply.github.com>
2020-12-27 16:41:16 +0300

    Club command (#2)
    * Start command add
    
    * Add vk libary, command /club

--- handlers.py
+++ handlers.py
@@ -1,3 +1,5 @@
+import vk
+import settings
 
 def start(update, context):
     print('Вазван/Start')
@@ -5,3 +7,13 @@ def start(update, context):
     update.message.reply_text(
         f'Привет {user_name}!'
     )
+
+def club(update, context):
+    print('Вазван/Club')
+    user_say = update.message.text.split()
+    if user_say[1].capitalize() == "Mutabor":
+        session = vk.Session(access_token=settings.VK_TOKEN)
+        api = vk.API(session, v=5.126)
+        res = api.wall.get(domain='mutabor.moscow', count= 1)
+        result = res['items'][0]['text']
+        update.message.reply_text(result)
