5f4f884d356dad99ee09f68e41d14b78615c081b
Jhon-G <74727160+Jhon-G@users.noreply.github.com>
2021-01-18 19:47:23 +0300

    Club command (#4)
    * Start command add
    
    * Add vk libary, command /club
    
    * conflict
    
    * Add keyboard, parser
    
    * fixed requests
    
    * fixed bot.log
    
    * Delete bot_log
    
    * add 2nd club and modified parse process
    
    * fixed with flake8

--- utils.py
+++ utils.py
@@ -1,13 +1,16 @@
 import vk
 import settings
-from telegram import ReplyKeyboardMarkup, KeyboardButton
+from telegram import ReplyKeyboardMarkup
+
 
 def session_api():
     session = vk.Session(access_token=settings.VK_TOKEN)
     api = vk.API(session, v=5.126)
     return api
 
+
 def main_keyboard():
     return ReplyKeyboardMarkup([
-        ['Mutabor']
+        ['Mutabor'],
+        ['Random']
     ])
