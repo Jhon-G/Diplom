4b5ad2ef0954c3245936ad113e09117344b1a2f8
Ivan <obuchenie_gurov@mail.ru>
2021-01-20 19:46:20 +0300

    work on mistakes

--- utils.py
+++ utils.py
@@ -4,7 +4,6 @@ import settings
 from telegram import ReplyKeyboardMarkup
 import redis3
 from rutimeparser import parse
-import logging
 
 
 def today_date():
@@ -25,18 +24,23 @@ def redis_server():
 
 def get_wall_posts(domain=''):
     api = session_api()
-    wall_posts = api.wall.get(domain=domain, count=10)  # domain is name of group https://vk.com/ "mutabor.moscow", count if how many posts from group we take
+    # domain is name of group https://vk.com/ "mutabor.moscow",
+    # count if how many posts from group we take
+    wall_posts = api.wall.get(domain=domain, count=10)
     posts = wall_posts['items']
     return posts
 
 
-def clear_posts_text_to_parse(domain=''):  # clear text from words who can give as ValueError by parser
+def clear_posts_text_to_parse(domain=''):
+    # clear text from words who can give as ValueError by parser
     posts = get_wall_posts(domain=domain)
     today = today_date()
     if 'rndm.club' in domain:
         for post in reversed(posts):
             post_text = post['text']
-            if '|' in post_text.replace('год', '').replace('лет', ''):  # we split text to create list where [1] object will be date "day(1) mounth(january)" and here we miss posts without "|" char
+            # we split text to create list where [1] object will be date ->
+            # "day(1) mounth(january)" and here we miss posts without "|" char
+            if '|' in post_text.replace('год', '').replace('лет', ''):
                 clear_concert_text = post_text.split('|', 2)
                 concert_date = parse(clear_concert_text[1], allowed_results=(date, None))
                 if concert_date >= today:
@@ -48,7 +52,8 @@ def clear_posts_text_to_parse(domain=''):  # clear text from words who can give 
     elif 'mutabor.moscow' in domain:
         for post in reversed(posts):
             post_text = post['text']
-            clear_concert_text = post_text.replace('лет', '').replace('лет', '')  # here we reaplace word 'years' becouse parser get mistake
+            # here we reaplace word 'years' becouse parser get mistake
+            clear_concert_text = post_text.replace('лет', '').replace('лет', '')
             concert_date = parse(clear_concert_text, allowed_results=(date, None))
             post_text, concert_date = correct_parsed_date(post_text, concert_date, today)
             if concert_date >= today:
@@ -60,7 +65,8 @@ def clear_posts_text_to_parse(domain=''):  # clear text from words who can give 
 
 def correct_parsed_date(post_text, concert_date, today):
     if concert_date is not None and type(concert_date) is date:
-        if concert_date.year != today.year:  # if parser find date with year == 2022 we correct this to 2021
+        # if parser find date with year == 2022 we correct this to 2021
+        if concert_date.year != today.year:
             correct_date = concert_date - timedelta(days=365)
             concert_date = correct_date
             return post_text, correct_date
